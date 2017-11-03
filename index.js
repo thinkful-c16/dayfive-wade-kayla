698cef7aaf2aa6a69da92c8cdfaeb248f0f021a5
Kayla Webb <kaylarwebb@gmail.com>
2017-11-03 11:50:48 -0400

    New and check item code working

--- index.js
+++ index.js
@@ -1,27 +1,32 @@
+/* global $ */
 'use strict';
 
 function addItems() {
+  //form is already loaded on the DOM//
   $('#js-shopping-list-form').submit(function(event){
     event.preventDefault();
-    let item = $('.js-shopping-list-entry').val();
-    $('.shopping-list').append(`<li><span class='shopping-item'>`+item+`</span>
-        <div class='shopping-item-controls'><button class='shopping-item-toggle'><span class='button-label'>`+'check'+`</span>
+    const newItem = $('.js-shopping-list-entry').val();
+    $('.shopping-list').append(`<li><span class='shopping-item'>${newItem}</span>
+        <div class='shopping-item-controls'><button class='shopping-item-toggle'><span class='button-label'>check</span>
             </button>
             <button class="shopping-item-delete">
-                <span class="button-label">`+'delete'+`</span>
+                <span class="button-label">delete</span>
             </button>
             </div>
             </li>`);
-    console.log(item);
+    $('.js-shopping-list-entry').val('');
 
-
-  })
+  });
 
 }
 
+// $('.js-parent-element').on('click', 'js-child-element', function())
+// .click(//)
+
 function checkItems() {
-  $('.shopping-item-toggle').on('click', function(event) {
-    $(this).toggleClass('shopping-item__checked');
+  //unordered list is fixed from the beginning and the toggle is its child//
+  $('.shopping-list').on('click', '.shopping-item-toggle', function(event) {
+    $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
   })
 }
 
@@ -29,6 +34,12 @@ function checkItems() {
 //     //code here
 // }
 
+function main() {
+  $(addItems)();
+  $(checkItems)();
+
+
+}
 
 $(addItems());
 $(checkItems());
