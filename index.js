125601dcab4103ca1f52fbb0abe9c1a5ed3f7697
Kayla Webb <kaylarwebb@gmail.com>
2017-11-03 14:47:21 -0400

    include new -js prefix on classes

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function addItems() {
   $('#js-shopping-list-form').submit(function(event){
     event.preventDefault();
     const newItem = $('.js-shopping-list-entry').val();
-    $('.shopping-list').append(`<li><span class='shopping-item'>${newItem}</span>
+    $('.js-shopping-list').append(`<li><span class='shopping-item'>${newItem}</span>
         <div class='shopping-item-controls'><button class='shopping-item-toggle'><span class='button-label'>check</span>
             </button>
             <button class="shopping-item-delete">
@@ -26,13 +26,13 @@ function addItems() {
 
 function checkItems() {
   //unordered list is fixed from the beginning and the toggle is its child//
-  $('.shopping-list').on('click', '.shopping-item-toggle', function(event) {
+  $('.js-shopping-list').on('click', '.shopping-item-toggle', function(event) {
     $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
   });
 }
 
 function deleteItems() {
-  $('.shopping-list').on('click', '.shopping-item-delete', function(event) {
+  $('.js-shopping-list').on('click', '.shopping-item-delete', function(event) {
     $(this).closest('li').remove();
 
   });
